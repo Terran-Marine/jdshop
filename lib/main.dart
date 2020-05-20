5a1d5484748f9c1584a16c0c9fc9516fc40c37eb
gongjian <chinagj@vip.qq.com>
2020-05-20 11:36:46 +0800

    feat: 1.商品列表 上拉刷新 下拉加载更多 2.分类刷新数据

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/physics.dart';
 import 'package:jdshop/tools/HttpTool.dart';
 import 'AppConfig.dart';
 import 'pages/Tabs.dart';
 import 'package:jdshop/routers/router.dart';
-
+import 'package:pull_to_refresh/pull_to_refresh.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(MyApp());
@@ -24,16 +26,62 @@ class _MyAppState extends State<MyApp> {
   }
 
   void _initHttpDio() {
-    dio.options.receiveTimeout=RECEIVE_TIMEOUT;//接收数据的最长时限
-    dio.options.baseUrl=BASE_URL;//请求基地址,可以包含子路径，如: "https://www.google.com/api/".
+    dio.options.receiveTimeout = RECEIVE_TIMEOUT; //接收数据的最长时限
+    dio.options.baseUrl =
+        BASE_URL; //请求基地址,可以包含子路径，如: "https://www.google.com/api/".
     dio.interceptors.add(LogInterceptor(responseBody: true)); //开启请求日志
   }
 
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      initialRoute: '/',
-      onGenerateRoute: onGenerateRoute,
+    return RefreshConfiguration(
+      headerBuilder: () => WaterDropHeader(),
+      // 配置默认头部指示器,假如你每个页面的头部指示器都一样的话,你需要设置这个
+      footerBuilder: () => ClassicFooter(),
+      // 配置默认底部指示器
+      headerTriggerDistance: 80.0,
+      // 头部触发刷新的越界距离
+      springDescription:
+          SpringDescription(stiffness: 170, damping: 16, mass: 1.9),
+      // 自定义回弹动画,三个属性值意义请查询flutter api
+      maxOverScrollExtent: 100,
+      //头部最大可以拖动的范围,如果发生冲出视图范围区域,请设置这个属性
+      maxUnderScrollExtent: 0,
+      // 底部最大可以拖动的范围
+      enableScrollWhenRefreshCompleted: true,
+      //这个属性不兼容PageView和TabBarView,如果你特别需要TabBarView左右滑动,你需要把它设置为true
+      enableLoadingWhenFailed: true,
+      //在加载失败的状态下,用户仍然可以通过手势上拉来触发加载更多
+      hideFooterWhenNotFull: false,
+      // Viewport不满一屏时,禁用上拉加载更多功能
+      enableBallisticLoad: true,
+      // 可以通过惯性滑动触发加载更多
+      child: MaterialApp(
+        localizationsDelegates: [
+          // this line is important
+          RefreshLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+
+        ],
+        supportedLocales: [
+          const Locale('en'),
+          const Locale('zh'),
+        ],
+        localeResolutionCallback:
+            (Locale locale, Iterable<Locale> supportedLocales) {
+          //print("change language");
+          return locale;
+        },
+        initialRoute: '/',
+        onGenerateRoute: onGenerateRoute,
+      ),
     );
+
+//    return MaterialApp(
+//      initialRoute: '/',
+//      onGenerateRoute: onGenerateRoute,
+//    );
   }
 }
--- lib/pages/product/ProductListPage.dart
+++ lib/pages/product/ProductListPage.dart
@@ -1,10 +1,13 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:jdshop/AppConfig.dart';
 import 'package:jdshop/model/ProductModel.dart';
 import 'package:jdshop/tools/HttpTool.dart';
 import 'package:jdshop/tools/ImageTool.dart';
+import 'package:loading_animations/loading_animations.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 class ProductListPage extends StatefulWidget {
   Map arguments;
@@ -19,15 +22,32 @@ class _ProductListPageState extends State<ProductListPage> {
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey();
   List<ProductItemModel> productList = [];
   int _page = 1;
-  String _sort = "all";
+  String _sort = "all_-1";
+  final int _pageSize = 10;
+
+  List _subHeaderList = [
+    {
+      "id": 1,
+      "title": "综合",
+      "fileds": "all",
+      "sort":
+          -1, //排序     升序：price_1     {price:1}        降序：price_-1   {price:-1}
+    },
+    {"id": 2, "title": "销量", "fileds": 'salecount', "sort": -1},
+    {"id": 3, "title": "价格", "fileds": 'price', "sort": -1},
+    {"id": 4, "title": "筛选"}
+  ];
 
   int _selectTagIndex = 0;
+
+  RefreshController _refreshController =
+      RefreshController(initialRefresh: false);
 
   @override
   void initState() {
     super.initState();
-
-    _getProductListData();
+    _getProductListData(false);
+    _selectTagIndex = _subHeaderList[0]['id'];
   }
 
   @override
@@ -64,187 +84,213 @@ class _ProductListPageState extends State<ProductListPage> {
                     width: 1, color: Color.fromRGBO(233, 233, 233, 0.9)))),
         width: double.infinity,
         child: Row(
-          children: <Widget>[
-            Expanded(
-              flex: 1,
-              child: InkWell(
-                child: Text(
-                  "综合",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      color:
-                          _selectTagIndex == 0 ? Colors.red : Colors.black54),
-                ),
-                onTap: () {
-                  setState(() {
-                    _selectTagIndex = 0;
-                  });
-                },
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: InkWell(
-                child: Text(
-                  "销量",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      color:
-                          _selectTagIndex == 1 ? Colors.red : Colors.black54),
-                ),
-                onTap: () {
-                  setState(() {
-                    _selectTagIndex = 1;
-                  });
-                },
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: InkWell(
-                child: Text(
-                  "价格",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      color:
-                          _selectTagIndex == 2 ? Colors.red : Colors.black54),
-                ),
-                onTap: () {
-                  setState(() {
-                    _selectTagIndex = 2;
-                  });
-                },
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: InkWell(
-                child: Text(
-                  "筛选",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      color:
-                          _selectTagIndex == 3 ? Colors.red : Colors.black54),
-                ),
-                onTap: () {
-                  _scaffoldKey.currentState.openEndDrawer();
-                  setState(() {
-                    _selectTagIndex = 3;
-                  });
-                },
-              ),
-            ),
-          ],
+          children: _getTagsWidgetList(),
         ),
       ),
     );
   }
 
+  void _tagsChange(e) {
+    if (e['id'] == 4) {
+      _scaffoldKey.currentState.openEndDrawer();
+    } else {
+      _sort = "${e['fileds']}_${e['sort']}";
+      _getProductListData(false);
+    }
+
+    setState(() {
+      print(e['id']);
+      _selectTagIndex = e['id'];
+    });
+  }
+
+  List<Widget> _getTagsWidgetList() {
+//    _selectTagIndex = _subHeaderList[0]['id'];
+    return _subHeaderList
+        .map((e) => Expanded(
+              flex: 1,
+              child: InkWell(
+                child: Text(
+                  e['title'],
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: _selectTagIndex == e['id']
+                          ? Colors.red
+                          : Colors.black54),
+                ),
+                onTap: () {
+                  _tagsChange(e);
+                },
+              ),
+            ))
+        .toList();
+  }
+
   Widget _productListWidget() {
-    return Container(
-      margin: EdgeInsets.only(top: ScreenUtil().setWidth(50)),
-      child: ListView.builder(
-        itemBuilder: (context, index) {
-          return Container(
-            padding: EdgeInsets.only(
-                left: ScreenUtil().setWidth(5),
-                right: ScreenUtil().setWidth(5)),
-//              color: index%2==0?Colors.blueGrey:Colors.white,
-            height: ScreenUtil().setWidth(130),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      height: ScreenUtil().setWidth(110),
-                      width: ScreenUtil().setWidth(110),
-                      child: Image.network(
-                        formatImageUrl(productList[index].pic),
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Container(
-                        padding:
-                            EdgeInsets.only(left: ScreenUtil().setWidth(5)),
-                        height: ScreenUtil().setWidth(110),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: <Widget>[
-                            Text(
-                              productList[index].title,
-                              maxLines: 2,
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                            Row(
-                              children: <Widget>[
-                                Container(
-                                    height: ScreenUtil().setWidth(30),
-                                    margin: EdgeInsets.only(right: 10),
-                                    padding: EdgeInsets.fromLTRB(10, 3, 10, 3),
-                                    //注意 如果Container里面加上decoration属性，这个时候color属性必须得放在BoxDecoration
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(10),
-                                      color: Color.fromRGBO(230, 230, 230, 0.9),
-                                    ),
-                                    child: Center(
-                                      child: Text("4g"),
-                                    )),
-                                Container(
-                                    height: ScreenUtil().setWidth(30),
-                                    margin: EdgeInsets.only(right: 10),
-                                    padding: EdgeInsets.fromLTRB(10, 3, 10, 3),
-                                    //注意 如果Container里面加上decoration属性，这个时候color属性必须得放在BoxDecoration
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(10),
-                                      color: Color.fromRGBO(230, 230, 230, 0.9),
-                                    ),
-                                    child: Center(
-                                      child: Text("256"),
-                                    )),
-                              ],
-                            ),
-                            Text(
-                              "￥${productList[index].price}",
-                              style: TextStyle(color: Colors.red, fontSize: 16),
-                            )
-                          ],
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-                Expanded(
-                  flex: 1,
-                  child: Container(),
-                ),
-                Divider(
-                  height: 1,
-                )
-              ],
+    return productList.length == 0
+        ? LoadingFlipping.circle()
+        : Container(
+            margin: EdgeInsets.only(top: ScreenUtil().setWidth(50)),
+            child: SmartRefresher(
+              enablePullDown: true,
+              enablePullUp: true,
+              header: WaterDropHeader(),
+              footer: CustomFooter(
+                builder: (BuildContext context, LoadStatus mode) {
+                  Widget body;
+                  if (mode == LoadStatus.idle) {
+                    body = Text("上拉加载");
+                  } else if (mode == LoadStatus.loading) {
+                    body = CupertinoActivityIndicator();
+                  } else if (mode == LoadStatus.failed) {
+                    body = Text("加载失败！点击重试！");
+                  } else if (mode == LoadStatus.canLoading) {
+                    body = Text("松手,加载更多!");
+                  } else {
+                    body = Text("没有更多数据了!");
+                  }
+                  return Container(
+                    height: 55.0,
+                    child: Center(child: body),
+                  );
+                },
+              ),
+              controller: _refreshController,
+              onRefresh: _onRefresh,
+              onLoading: _onLoading,
+              child: ListView.builder(
+                itemBuilder: (context, index) {
+                  return _productItemWidget(index);
+                },
+                itemCount: productList.length,
+              ),
             ),
           );
-        },
-        itemCount: productList.length,
+  }
+
+  Container _productItemWidget(int index) {
+    return Container(
+      padding: EdgeInsets.only(
+          left: ScreenUtil().setWidth(5), right: ScreenUtil().setWidth(5)),
+//              color: index%2==0?Colors.blueGrey:Colors.white,
+      height: ScreenUtil().setWidth(130),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                height: ScreenUtil().setWidth(110),
+                width: ScreenUtil().setWidth(110),
+                child: Image.network(
+                  formatImageUrl(productList[index].pic),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Container(
+                  padding: EdgeInsets.only(left: ScreenUtil().setWidth(5)),
+                  height: ScreenUtil().setWidth(110),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Text(
+                        productList[index].title,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      Row(
+                        children: <Widget>[
+                          Container(
+                              height: ScreenUtil().setWidth(30),
+                              margin: EdgeInsets.only(right: 10),
+                              padding: EdgeInsets.fromLTRB(10, 3, 10, 3),
+                              //注意 如果Container里面加上decoration属性，这个时候color属性必须得放在BoxDecoration
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: Color.fromRGBO(230, 230, 230, 0.9),
+                              ),
+                              child: Center(
+                                child: Text("4g"),
+                              )),
+                          Container(
+                              height: ScreenUtil().setWidth(30),
+                              margin: EdgeInsets.only(right: 10),
+                              padding: EdgeInsets.fromLTRB(10, 3, 10, 3),
+                              //注意 如果Container里面加上decoration属性，这个时候color属性必须得放在BoxDecoration
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: Color.fromRGBO(230, 230, 230, 0.9),
+                              ),
+                              child: Center(
+                                child: Text("256"),
+                              )),
+                        ],
+                      ),
+                      Text(
+                        "￥ ${productList[index].price}",
+                        style: TextStyle(color: Colors.red, fontSize: 16),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(),
+          ),
+          Divider(
+            height: 1,
+          )
+        ],
       ),
     );
   }
 
-  void _getProductListData() async {
+  void _onRefresh() async {
+    _getProductListData(false);
+  }
+
+  void _onLoading() async {
+    _getProductListData(true);
+  }
+
+  void _getProductListData(bool isMore) async {
+    if (isMore) {
+      _page++;
+    } else {
+      _page = 1;
+      productList.clear();
+    }
+
     Response respons = await dio.get(API_PLIST, queryParameters: {
       "cid": widget.arguments['cid'],
       "page": _page,
-//      "sort": _sort,
+      "pageSize": _pageSize,
+      "sort": _sort,
     });
 
     if (respons.statusCode == 200 && respons.data != null) {
       ProductModel productModel = ProductModel.fromJson(respons.data);
       setState(() {
-        productList = productModel.result;
+        if (isMore) {
+          if (productModel.result.length < _pageSize) {
+            _refreshController.loadNoData();
+          } else {
+            _refreshController.loadComplete();
+          }
+        } else {
+          _refreshController.refreshCompleted();
+          //刷新之后把状态设置位初始状态,否则不能再次加载更多
+          _refreshController.footerMode.value = LoadStatus.idle;
+        }
+
+        productList.addAll(productModel.result);
       });
     }
   }
